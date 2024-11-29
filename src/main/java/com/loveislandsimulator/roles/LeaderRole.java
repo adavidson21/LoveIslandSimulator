c5b163b878de3a9fcbede84992e203fa00538488
Alexis Davidson <alexis.davidson221@gmail.com>
2024-11-29 16:01:33 -0500

    Add role selection on challenge selection.

--- src/main/java/com/loveislandsimulator/roles/LeaderRole.java
+++ src/main/java/com/loveislandsimulator/roles/LeaderRole.java
@@ -3,6 +3,7 @@ package com.loveislandsimulator.roles;
 import com.loveislandsimulator.decorators.RoleDecorator;
 import com.loveislandsimulator.enums.Role;
 import com.loveislandsimulator.models.ChallengeCommand;
+import com.loveislandsimulator.models.GameData;
 import com.loveislandsimulator.models.Islander;
 
 public class LeaderRole extends RoleDecorator {
@@ -14,6 +15,8 @@ public class LeaderRole extends RoleDecorator {
     @Override
     public void participateInChallenge(ChallengeCommand challenge) {
         super.participateInChallenge(challenge);
-        System.out.println(islander.getName() + ", " + Role.getRoleName(Role.LEADER));
+        islander.addPoints(10);
+        String message = this.islander.getName() + " applied Leader role for 10 extra points...";
+        GameData.getInstance().addLogMessage(message);
     }
 }
